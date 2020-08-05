060a4d661c0de5270bbff7dc5bab49996c161ade
qianhao <ianloe666@gmail.com>
2020-08-05 11:02:36 -0500

    reload

--- auth/server/index.js
+++ auth/server/index.js
@@ -12,18 +12,25 @@ const cors = require('cors')
 // DB setup
 // mongoose.connect('mongodb://localhost:2701/1', { useNewUrlParser: true })
 // mongoose.connect('mongodb://localhost:27017/react-redux', {useNewUrlParser: true});
-mongoose.connect(
-    "mongodb://localhost:27017/auth-real", 
-    { 
-        useNewUrlParser: true,
-        useUnifiedTopology: true 
-    }
-);
-const connection = mongoose.connection;
+// const url = "mongodb+srv://qianhao:<password>@cluster0.jxmey.mongodb.net/<dbname>?retryWrites=true&w=majority"
 
-connection.on("connected", function() {
-  console.log("connected to db");
-});
+const connection = "mongodb+srv://qianhao:6652167@cluster0.jxmey.mongodb.net/react-redux?retryWrites=true&w=majority";
+
+mongoose.connect(connection,{ useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false})
+    .then(() => console.log("Database Connected Successfully"))
+    .catch(err => console.log(err));
+// mongoose.connect(
+//     "mongodb://localhost:27017/auth-real", 
+//     { 
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true 
+//     }
+// // );
+// const connection = mongoose.connection;
+
+// connection.on("connected", function() {
+//   console.log("connected to db");
+// });
 
 // App setup
 // income request will be passed into these 2 parts
